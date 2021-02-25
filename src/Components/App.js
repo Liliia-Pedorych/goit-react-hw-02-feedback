be075111b2847b89e814113971bbafb9ad41735e
Liliia Pedorych <liliapedorych@ukr.net>
2021-02-25 21:22:21 +0200

    Додає початкові файли

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from './Container/Container';
+
+const App = () => {
+  return (
+    <Container>
+      {/* //     <Profile */}
+      {/* //       name={user.name}
+  //       tag={user.tag}
+  //       location={user.location}
+  //       avatar={user.avatar}
+  //       stats={user.stats}
+  //     />
+  //     <Statistics title="Upload stats" stats={statisticalData} />
+  //     <FriendList friends={friends} />
+  //     <TransactionHistory items={transactions} /> */}
+    </Container>
+  );
+};
+
+export default App;
