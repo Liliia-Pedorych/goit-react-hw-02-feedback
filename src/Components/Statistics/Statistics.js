549405564f11d89cdd2306914d85f43219a0a75c
Liliia Pedorych <liliapedorych@ukr.net>
2021-02-26 21:56:43 +0200

    виконує 4 умову задачі

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import Notification from '../Notification/Notification';
 // // import './Statistics.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  const isFeedback = total === 0;
   return (
     <>
-      <h2>Statistics</h2>
-      <p>Good:{good}</p>
-      <p>Neutral:{neutral}</p>
-      <p>Bad:{bad}</p>
-      <p>Total:{total}</p>
-      <p>Positive feedback:{positivePercentage}%</p>
+      {isFeedback ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <>
+          <h2>Statistics</h2>
+          <p>Good:{good}</p>
+          <p>Neutral:{neutral}</p>
+          <p>Bad:{bad}</p>
+          <p>Total:{total}</p>
+          <p>Positive feedback:{positivePercentage}%</p>
+        </>
+      )}
     </>
   );
 };
